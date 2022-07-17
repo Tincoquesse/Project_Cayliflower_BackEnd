f3c6851dc4e6015991f554993763f96d46ab95d0
Tincoquesse <kamil.sound@gmail.com>
2022-07-17 14:53:05 +0200

    implemented adding and retrieving Category

--- src/main/java/com/inqoo/project_cayliflower_backend/repository/TrainingRepo.java
+++ src/main/java/com/inqoo/project_cayliflower_backend/repository/TrainingRepo.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface TrainingRepo extends JpaRepository<Training, Long> {
 
-    Optional<Training> findAllByName(String name);
+    Optional<Training> findByName(String name);
 }
