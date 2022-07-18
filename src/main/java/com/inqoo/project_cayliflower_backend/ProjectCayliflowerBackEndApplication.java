97698fcabd93add7daf2c15006145eedb4e857a6
Tincoquesse <kamil.sound@gmail.com>
2022-07-18 06:54:44 +0200

    Mock categories on startup

--- src/main/java/com/inqoo/project_cayliflower_backend/ProjectCayliflowerBackEndApplication.java
+++ src/main/java/com/inqoo/project_cayliflower_backend/ProjectCayliflowerBackEndApplication.java
@@ -1,13 +1,34 @@
 package com.inqoo.project_cayliflower_backend;
 
+import com.inqoo.project_cayliflower_backend.configuration.controller.CauliflowerRestController;
+import com.inqoo.project_cayliflower_backend.model.CategoryDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ProjectCayliflowerBackEndApplication {
+
+    CauliflowerRestController controller;
+
+    public ProjectCayliflowerBackEndApplication(CauliflowerRestController controller) {
+        this.controller = controller;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectCayliflowerBackEndApplication.class, args);
     }
 
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        controller.addCategory(new CategoryDTO("IT", "IT Categoty", Collections.emptyList()));
+        controller.addCategory(new CategoryDTO("Sales", "Sales Categoty", Collections.emptyList()));
+        controller.addCategory(new CategoryDTO("Marketing", "Marketing Categoty", Collections.emptyList()));
+        controller.addCategory(new CategoryDTO("Other", "Other Categoty", Collections.emptyList()));
+    }
 }
