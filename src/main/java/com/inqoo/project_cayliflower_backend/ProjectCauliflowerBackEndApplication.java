aedf9f8882244bc09367cfa22e9ff1df6310e9b8
Tincoquesse <kamil.sound@gmail.com>
2022-07-21 22:10:41 +0200

    implemented adding and retrieving Category

--- src/main/java/com/inqoo/project_cayliflower_backend/ProjectCauliflowerBackEndApplication.java
+++ src/main/java/com/inqoo/project_cayliflower_backend/ProjectCauliflowerBackEndApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectCayliflowerBackEndApplication {
+public class ProjectCauliflowerBackEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectCayliflowerBackEndApplication.class, args);
+        SpringApplication.run(ProjectCauliflowerBackEndApplication.class, args);
     }
 
 }
