11fe1c54301ccf5d64b6995f2aff078cbc77ff6f
Tincoquesse <kamil.sound@gmail.com>
2022-07-23 09:55:58 +0200

    Profile "prod" added to run configuration

--- src/main/java/com/inqoo/project_cayliflower_backend/ProjectCayliflowerBackEndApplication.java
+++ src/main/java/com/inqoo/project_cayliflower_backend/ProjectCayliflowerBackEndApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectCayliflowerBackEndApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectCayliflowerBackEndApplication.class, args);
     }
