32312abdfe7b5170dce1744fbd44f151a22d6631
PawelGajec <gajec.pawel@gmail.com>
2022-09-25 13:38:17 +0200

    V 1.3

--- src/main/java/com/inqoo/project_cayliflower_backend/configuration/DbFilling.java
+++ src/main/java/com/inqoo/project_cayliflower_backend/configuration/DbFilling.java
@@ -44,6 +44,8 @@ public class DbFilling {
         controller.addSubcategory(new SubcategoryDTO("JS"),"IT");
         controller.addSubcategory(new SubcategoryDTO("DevOps"),"IT");
 
+        //Java
+
         controller.addTraining(new TrainingDTO("Java Podstawy","Kurs skierowany do osób, " +
                 "chcących rozpocząć swoją przygodę z branżą IT, nie wymaga wcześniejszego " +
                 "doświadczenia w programowaniu",
@@ -68,5 +70,74 @@ public class DbFilling {
         controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Hibernate", "Kamil", "Kotlarz", new HashSet<>()));
         controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Hibernate", "Piotr", "Laskowski", new HashSet<>()));
         controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Spring", "Piotr", "Laskowski", new HashSet<>()));
+
+        //C Sharp
+
+        controller.addTraining(new TrainingDTO("C Sharp Podstawy","Kurs skierowany do osób, " +
+                "chcących rozpocząć swoją przygodę z branżą IT, nie wymaga wcześniejszego " +
+                "doświadczenia w programowaniu",
+                new BigDecimal(3000),20,new HashSet<>(),new HashSet<>()),"C Sharp");
+        controller.addTraining(new TrainingDTO("C Sharp Zaawansowany","Szkolenie dla osób które posiadają doświadczenie w branży IT, " +
+                "chcących zgłębiać tajniki języka C # ",
+                new BigDecimal(3000),25,new HashSet<>(),new HashSet<>()),"C Sharp");
+        controller.addTraining(new TrainingDTO("C Sharp UI","Spring to najpopularniejszy framework dla Java, dlatego jego znajomość jest " +
+                "praktycznie niezbędna, jeśli myślisz o karierze Java Developera.",
+                new BigDecimal(2500),15,new HashSet<>(),new HashSet<>()),"C Sharp");
+        controller.addTraining(new TrainingDTO("C Sharp Application Security","Hibernate framework stanowiący jedną z najpopularniejszych implementacji Java Persistence API (JPA)." +
+                "Odpowiada za wygodna komunikację aplikacji z bazą danych.",
+                new BigDecimal(2500),15,new HashSet<>(),new HashSet<>()),"C Sharp");
+
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("C Sharp Podstawy", "Paweł", "Gajec", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("C Sharp Zaawansowany", "Piotr", "Laskowski", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("C Sharp Zaawansowany", "Kamil", "Kotlarz", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("C Sharp UI", "Kamil", "Kotlarz", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("C Sharp Application Security", "Piotr", "Laskowski", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("C Sharp Application Security", "Paweł", "Gajec", new HashSet<>()));
+
+        //JS
+
+        controller.addTraining(new TrainingDTO("JS Podstawy","Kurs skierowany do osób, " +
+                "chcących rozpocząć swoją przygodę z branżą IT, nie wymaga wcześniejszego " +
+                "doświadczenia w programowaniu",
+                new BigDecimal(3000),20,new HashSet<>(),new HashSet<>()),"JS");
+        controller.addTraining(new TrainingDTO("JS Zaawansowany","Szkolenie dla osób które posiadają doświadczenie w branży IT, " +
+                "chcących zgłębiać tajniki języka JS ",
+                new BigDecimal(3000),25,new HashSet<>(),new HashSet<>()),"JS");
+        controller.addTraining(new TrainingDTO("Angular","Angular to kompleksowy framework do projektowania oraz tworzenia wydajnych aplikacji typu SPA (Single Page Application) . " +
+                "Napisany w języku TypeScript i wspierany oraz rozwijany przez Google.",
+                new BigDecimal(2500),15,new HashSet<>(),new HashSet<>()),"JS");
+        controller.addTraining(new TrainingDTO("React","React jest nazywany JavaScriptową biblioteką, służącą do budowania interfejsów dla użytkownika. " +
+                "Umożliwia tworzenie złożonych interfejsów za pomocą małych, odizolowanych od siebie komponentów",
+                new BigDecimal(2500),15,new HashSet<>(),new HashSet<>()),"JS");
+
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("JS Podstawy", "Paweł", "Gajec", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("JS Zaawansowany", "Piotr", "Laskowski", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("JS Zaawansowany", "Kamil", "Kotlarz", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Angular", "Kamil", "Kotlarz", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Angular", "Piotr", "Laskowski", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("React", "Paweł", "Gajec", new HashSet<>()));
+
+        //Dav Ops
+
+        controller.addTraining(new TrainingDTO("Dev Ops Podstawy","Kurs skierowany do osób, " +
+                "chcących rozpocząć swoją przygodę z branżą IT, nie wymaga wcześniejszego " +
+                "doświadczenia w programowaniu",
+                new BigDecimal(3000),20,new HashSet<>(),new HashSet<>()),"DevOps");
+        controller.addTraining(new TrainingDTO("Dev Ops Zaawansowany","Szkolenie dla osób które posiadają doświadczenie w branży IT, " +
+                "chcących zgłębiać tajniki języka C # ",
+                new BigDecimal(3000),25,new HashSet<>(),new HashSet<>()),"DevOps");
+        controller.addTraining(new TrainingDTO("Azure Dev Ops","Spring to najpopularniejszy framework dla Java, dlatego jego znajomość jest " +
+                "praktycznie niezbędna, jeśli myślisz o karierze Java Developera.",
+                new BigDecimal(2500),15,new HashSet<>(),new HashSet<>()),"DevOps");
+        controller.addTraining(new TrainingDTO("Jenkins Dev Ops","Hibernate framework stanowiący jedną z najpopularniejszych implementacji Java Persistence API (JPA)." +
+                "Odpowiada za wygodna komunikację aplikacji z bazą danych.",
+                new BigDecimal(2500),15,new HashSet<>(),new HashSet<>()),"DevOps");
+
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Dev Ops Podstawy", "Paweł", "Gajec", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Dev Ops Zaawansowany", "Piotr", "Laskowski", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Dev Ops Zaawansowany", "Kamil", "Kotlarz", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Azure Dev Ops", "Kamil", "Kotlarz", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Jenkins Dev Ops", "Piotr", "Laskowski", new HashSet<>()));
+        controller.assignTrainerToTraining(new TrainerToTrainingAssigmentDTO("Jenkins Dev Ops", "Paweł", "Gajec", new HashSet<>()));
     }
 }
